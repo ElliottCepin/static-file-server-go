57e1cf24c1257f682e258adf941eb9faf9fac3ec
ElliottCepin <elliottcepin5@gmail.com>
2026-01-01 18:48:36 -0700

    Working fileserver

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"log"
 )
 func main() {
+	http.Handle("/public/", http.FileServer(http.Dir(".")))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "You've laneded on the home page - there is nothing to see here")
 	})
 	
-	
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }	
