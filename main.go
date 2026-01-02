963a0df79877b7b64f6d29542c93a00e501ffef8
ElliottCepin <elliottcepin5@gmail.com>
2026-01-01 18:16:46 -0700

    server working

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
 func main() {
-	fmt.Println("Hello World");
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "You've laneded on the home page - there is nothing to see here")
+	})
+	
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}	
